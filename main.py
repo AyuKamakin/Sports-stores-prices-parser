ede9f480e2f2178f9585316fb77d5c0b05ad4083
AyuKamakin <101467421+AyuKamakin@users.noreply.github.com>
2024-08-08 15:59:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -26,4 +26,48 @@ def get_price_sport_maraphon(url:str):
         mess=f'Не удалось получить страницу, статус код: {response.status_code}'
     return min(price,discount_price), mess
 
-print(get_price_sport_maraphon('https://sport-marafon.ru/catalog/muzhskie-uteplennye-kurtki-i-pukhovki/kurtka-muzhskaya-sivera-bekhterets-chili/'))
+
+def get_price_trial_sport(url: str):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+    }
+    price, discount_price = 0, 0
+    mess = 'Успешно получена цена'
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        # Поиск скрипта, содержащего нужные данные
+        script_tag = soup.find('script', string=re.compile(r'var eventParams = \{'))
+        if script_tag and script_tag.string:
+            # Извлечение JSON-строки из JavaScript
+            json_text_match = re.search(r'var eventParams = (\{.*?\});', script_tag.string, re.DOTALL)
+            if json_text_match:
+                json_text = json_text_match.group(1)
+                try:
+                    # Парсинг JSON
+                    data = json.loads(json_text)
+
+                    # Извлечение информации о продуктах
+                    products = data.get('products', [])
+                    if products:
+                        product = products[0]
+                        price = product.get('price', 0)
+                        discount_price = product.get('price_old', 0)
+                        print('Цена товара:', price)
+                        print('Старая цена товара:', discount_price)
+                    else:
+                        mess = 'Не удалось найти информацию о продуктах на странице'
+                except json.JSONDecodeError:
+                    mess = 'Ошибка при разборе JSON'
+            else:
+                mess = 'Не удалось извлечь JSON из JavaScript'
+        else:
+            mess = 'Не удалось найти нужные данные на странице'
+    else:
+        mess = f'Не удалось получить страницу, статус код: {response.status_code}'
+
+    return min(price, discount_price), mess
+
+print(get_price_trial_sport('https://trial-sport.ru/goods/2660343.html'))
